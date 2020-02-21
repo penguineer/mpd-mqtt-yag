2740ac40a86c991560c6fad0db0568b91214a6b8
Stefan Haun <tux@netz39.de>
2020-02-21 19:00:12 +0100

    Revert idle-timeout

--- mpd-mqtt-yag.py
+++ mpd-mqtt-yag.py
@@ -68,7 +68,7 @@ class MpdClientPool:
     def _create_client(self):
         client = MPDClient()
         client.timeout = 10
-        client.idletimeout = 10
+        client.idletimeout = None
         if self.password:
             client.password(self.password)
 
